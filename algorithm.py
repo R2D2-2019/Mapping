3a3c10daaf1d85923cdcf71905ce5396100d41f1
Tonyvdk <t.r.vanderkrogt@gmail.com>
2019-06-25 13:31:52 +0200

    Added documentation to the functions time_degrees_to_decimal_degrees and decimal_degrees_to_time_degrees

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,7 @@
 class Algorithms():
+    ##  Documentation for decimal_degrees_to_time_degrees
+    #   The function asks for a degree value from 0 till 360.
+    #   It then returns the Time degrees in a dict. 
     def decimal_degrees_to_time_degrees(self, decimal_degrees):
         assert decimal_degrees >=0 and decimal_degrees <= 360, "decimal degrees is out of scope"
         time_degrees = {"hours": 0, "minutes": 0, "seconds": 0}
@@ -6,7 +9,12 @@ class Algorithms():
         time_degrees["minutes"] = int((decimal_degrees - time_degrees["hours"])*60)
         time_degrees["seconds"] = round((decimal_degrees - time_degrees["hours"] - time_degrees["minutes"] / 60) * 3600)
         return time_degrees
-
+    
+    ##  Documentation for time_degrees_to_decimal_degrees
+    #   The function asks for a dict with a hours value from 0 till 360,
+    #   a minutes value from 0 till 59 and a second value from 0 till 59.
+    #   If the hours value is 360 it will set the minutes value and seconds value to 0.
+    #   It then returns the decimal degrees in a float
     def time_degrees_to_decimal_degrees(self, time):
         assert time["hours"] >= 0 and time["hours"] <= 360, "Time degrees 'hours' is out of scope"
         assert time["minutes"] >= 0 and time["minutes"] < 60 , "Time degrees 'minutes' is out of scope"
