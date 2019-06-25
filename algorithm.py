ff273242773d0b9418f481d100c27f1d02961694
Tonyvdk <t.r.vanderkrogt@gmail.com>
2019-06-25 13:17:08 +0200

    Made a function to convert decimal degrees to time degrees

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,12 @@
 class Algorithms():
+    def decimal_degrees_to_time_degrees(self, decimal_degrees):
+        assert decimal_degrees >=0 and decimal_degrees <= 360, "decimal degrees is out of scope"
+        time_degrees = {"hours": 0, "minutes": 0, "seconds": 0}
+        time_degrees["hours"] = int(decimal_degrees)
+        time_degrees["minutes"] = int((decimal_degrees - time_degrees["hours"])*60)
+        time_degrees["seconds"] = round((decimal_degrees - time_degrees["hours"] - time_degrees["minutes"] / 60) * 3600)
+        return time_degrees
+
     def time_degrees_to_decimal_degrees(self, time):
         assert time["hours"] >= 0 and time["hours"] <= 360, "Time degrees 'hours' is out of scope"
         assert time["minutes"] >= 0 and time["minutes"] < 60 , "Time degrees 'minutes' is out of scope"
