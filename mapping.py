a22617f94823d9e3e16b342c269e89ef249ef4bf
Dattax05 <36602654+Dattax05@users.noreply.github.com>
2019-06-28 16:05:05 +0200

    Added update_robot_location to test location changes when robot is moving

--- mapping.py
+++ mapping.py
@@ -2,7 +2,11 @@ from algorithm import Algorithms
 class mapping:
     def __init__(self):
         self.current_location = [10, 10]#*2
-        self.lidar_data =   [200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,
+        self.new_location = [10.0004,10.0004]
+        self.coordinates_movement = [0,0]
+        self.robot_movement_x = 0
+        self.robot_movement_y = 0
+        self.lidar_data =   [200,200,200,200,200,200,200,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,20,200,200,200,200,
                             200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,
                             200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,
                             200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,200,
@@ -20,7 +24,27 @@ class mapping:
         assert type(which_degree) is int, "which_degree invalid type"
         lidar_distance = self.lidar_data[which_degree]
         distances = Algorithms().from_lidar_to_coordinate(which_degree, lidar_distance)
-        print(distances)
+        #print(distances)
         return distances
         #return Algorithms().calculate_new_coordinate(self.current_location[0], self.current_location[1], distances[0], distances[1] )
         
+    def update_robot_location(self, coordinate):
+        
+        self.robot_movement_x += Algorithms().measure_distance(self.current_location[0], self.current_location[1], self.current_location[0], self.new_location[0]) # meters
+        self.robot_movement_y += Algorithms().measure_distance(self.current_location[0], self.current_location[1], self.new_location[1], self.current_location[1])
+        if self.current_location[0] < self.new_location[0] and self.current_location[1] < self.new_location[1]: # x+y
+            coordinate[0] += round(self.robot_movement_x)
+            coordinate[1] += round(self.robot_movement_y)
+        elif self.current_location[0] < self.new_location[0]: # x+
+            coordinate[0] += round(self.robot_movement_x)
+        elif self.current_location[1] < self.new_location[1]: # y+
+            coordinate[1] += round(self.robot_movement_y)
+        elif self.current_location[0] > self.new_location[0] and self.current_location[1] > self.new_location[1]: # x-y
+            coordinate[0] -= round(self.robot_movement_x)
+            coordinate[1] -= round(self.robot_movement_y)
+        elif self.current_location[0] > self.new_location[0]: # x-
+            coordinate[0] -= round(self.robot_movement_x)
+        elif self.current_location[1] > self.new_location[1]: # y- 
+            coordinate[1] -= round(self.robot_movement_y)
+        self.current_location = self.new_location
+
