b3e6c26a896bb0222e87c3c92fed9e383efa1af1
Dattax05 <36602654+Dattax05@users.noreply.github.com>
2019-06-28 11:14:33 +0200

    Edited the test so it works with the new mapping class

--- test_maps.py
+++ test_maps.py
@@ -4,23 +4,24 @@ from list_map import ListMap
 from quadtree_map import QuadtreeMap
 from quadtree_map import Rectangle
 from cartesian_coordinate import CartesianCoordinate
+from mapping import mapping
 
 maps = []
 maps.append(KeyValueMap())
 maps.append(ListMap())
 
-root_size = Rectangle(0, 0, 20, 20)
+root_size = Rectangle(0, 0, 0, 0)
 maps.append(QuadtreeMap(root_size, 4))
 points = []
-points.append(CartesianCoordinate(-11, -11))
-points.append(CartesianCoordinate(0, 0))
-points.append(CartesianCoordinate(11, 11))
-points.append(CartesianCoordinate(-1, 1))
-points.append(CartesianCoordinate(1, -1))
+
+for degree in range(0, 360):
+    if mapping().lidar_data[degree] < 50: #For now 5 in meters
+        temp = mapping().place_obstacle_on_map(degree)
+        points.append(CartesianCoordinate(temp[0], temp[1]))
 
 for map in maps:
     for point in points:
         map.add_point_cartesian(point, PointState.occupied)
-
+    
     print(map.get_map_points())
     print(map)
